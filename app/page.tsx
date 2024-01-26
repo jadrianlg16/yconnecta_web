f39192cc46fbb420468a6c61941a1b10437f09ad
José Carlos Sánchez <a01174050@tec.mx>
2024-01-26 13:08:42 -0600

    avances navbar
    hice avances en el layout de dashboard admin, creando la barra de navbar, y reorganizando un poco los estilos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="h-full">
+    <main className="h-full d-flex flex-col w-full">
+      <Navbar links={["hello", "world"]} />
       <h1>hello world</h1>
     </main>
   );
---
+++ utils/constants.tsx
@@ -0,0 +1,22 @@
+export const adminDashbaordRoutes = [
+  {
+    name: "Dashboard",
+    path: "/admin",
+    icon: "fi fi-rr-chart-line-up",
+  },
+  {
+    name: "Publicaciones",
+    path: "/admin/publicaciones",
+    icon: "fi fi-rr-newspaper",
+  },
+  {
+    name: "Comunidad",
+    path: "/admin/comunidad",
+    icon: "fi fi-rr-users",
+  },
+  {
+    name: "Configuración",
+    path: "/admin/configuracion",
+    icon: "fi fi-rr-settings",
+  },
+];
